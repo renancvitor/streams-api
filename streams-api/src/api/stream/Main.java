bdb5d02f04ce25af4f0ead4a5ac0e1ca0c3e1094
renancvitor <renan.vitor.cm@gmail.com>
2025-07-03 11:02:52 -0300

    feat(curso): aula sobre interfaces funcionais - parte 2

--- streams-api/src/api/stream/Main.java
+++ streams-api/src/api/stream/Main.java
@@ -2,9 +2,8 @@ package api.stream;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BinaryOperator;
-import java.util.function.Consumer;
-import java.util.function.Function;
+import java.util.Random;
+import java.util.function.*;
 
 public class Main {
 
@@ -37,20 +36,20 @@ public class Main {
         System.out.println("Salário total: R$ " + salarioTotal2);
 
         // Consumer
-        System.out.println("Consumer - entra parâmetro e não retorna nada");
+        System.out.println("Consumer - entra parâmetro e não retorna nada:");
         Consumer<Empregado> consumer = (emp) -> {
             System.out.println(emp.getNome() + ", R$ " + String.format("%.2f",emp.getSalario()));
         };
         consumer.accept(new Empregado(10, "TreinaWeb", 1000, "Educação"));
 
         // Function
-        System.out.println("Function");
+        System.out.println("Function:");
         Function<Empregado, Double> function = (emp) -> emp.getSalario() * 10;
         double novoSalario = function.apply(new Empregado(100, "TreinaWeb10", 2000, "Educação"));
         System.out.println(novoSalario);
 
         // BinaryOperator
-        System.out.println("BinaryOperator");
+        System.out.println("BinaryOperator:");
         BinaryOperator<Empregado> binaryOperator = (emp1, emp2) -> new Empregado(
                 -1, emp1.getNome() + emp2.getNome(),
                 emp1.getSalario() + emp2.getSalario(),
@@ -66,5 +65,23 @@ public class Main {
         ));
         System.out.println(novoEmpregado.getNome() + ", R$ " + novoEmpregado.getSalario()
                 + ", Departamento: " + novoEmpregado.getDepartamento());
+
+        // Predicate
+        System.out.println("Predicate:");
+        Predicate<Empregado> predicate = (emp) -> emp.getNome().endsWith("Web");
+        boolean ternaComWeb = predicate.test(new Empregado(0, "TreinaWeb", 0, ""));
+        System.out.println(ternaComWeb);
+
+        // Supplier
+        System.out.println("Supplier: ");
+        Supplier<Empregado> supplier = () -> new Empregado(
+                new Random().nextInt(),
+                "TreinaWeb",
+                0,
+                "Departamnto");
+        Empregado emp1 = supplier.get();
+        System.out.println(emp1.getId());
+        Empregado emp2 = supplier.get();
+        System.out.println(emp2.getId());
     }
 }
