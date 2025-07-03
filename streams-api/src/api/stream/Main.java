45ba3f2967587bcccc59928bb6ee47409f156aeb
renancvitor <renan.vitor.cm@gmail.com>
2025-07-03 10:54:42 -0300

    feat(curso): aula sobre interfaces funcionais - parte 1

--- streams-api/src/api/stream/Main.java
+++ streams-api/src/api/stream/Main.java
@@ -2,6 +2,9 @@ package api.stream;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class Main {
 
@@ -32,5 +35,36 @@ public class Main {
         });
         double salarioTotal2 = empregados.stream().mapToDouble(emp -> emp.getSalario()).sum();
         System.out.println("Salário total: R$ " + salarioTotal2);
+
+        // Consumer
+        System.out.println("Consumer - entra parâmetro e não retorna nada");
+        Consumer<Empregado> consumer = (emp) -> {
+            System.out.println(emp.getNome() + ", R$ " + String.format("%.2f",emp.getSalario()));
+        };
+        consumer.accept(new Empregado(10, "TreinaWeb", 1000, "Educação"));
+
+        // Function
+        System.out.println("Function");
+        Function<Empregado, Double> function = (emp) -> emp.getSalario() * 10;
+        double novoSalario = function.apply(new Empregado(100, "TreinaWeb10", 2000, "Educação"));
+        System.out.println(novoSalario);
+
+        // BinaryOperator
+        System.out.println("BinaryOperator");
+        BinaryOperator<Empregado> binaryOperator = (emp1, emp2) -> new Empregado(
+                -1, emp1.getNome() + emp2.getNome(),
+                emp1.getSalario() + emp2.getSalario(),
+                emp1.getDepartamento() + emp2. getDepartamento());
+        Empregado novoEmpregado = binaryOperator.apply(new Empregado(
+                0, "Treima",
+                1000,
+                "Depa"), new Empregado(
+                        0,
+                        "Web",
+                        500,
+                        "rtamento"
+        ));
+        System.out.println(novoEmpregado.getNome() + ", R$ " + novoEmpregado.getSalario()
+                + ", Departamento: " + novoEmpregado.getDepartamento());
     }
 }
